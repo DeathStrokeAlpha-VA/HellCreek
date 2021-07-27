deae62bb1745cd495cce416b1d17ec4348420d26
Akshay Sharma <akshay@virtualanalytics.ai>
2021-07-27 10:36:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HellCreek
+
+Developed with Unreal Engine 4
---
+++ Source/HellCreek/HellCreekGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HellCreekGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELLCREEK_API AHellCreekGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
